47fcc4edbd293f5e7170d180c2a3afa715e92cd9
Dmytro Overchenko <dimzello@gmail.com>
2026-07-19 17:15:18 +0300

    Added write history on exit

--- src/ExitCommand.cpp
+++ src/ExitCommand.cpp
@@ -1,4 +1,5 @@
 #include "ExitCommand.h"
+#include "HistoryManager.h"
 
 ExitCommand::ExitCommand(const std::vector<std::string>& params) :
 	Command(CommandType::EXIT, params)
@@ -10,6 +11,11 @@ int ExitCommand::execute() const
 	if (m_params.size() > 1)
 	{
 		code = std::atoi(m_params[1].c_str());
+	}
+	const char* historyFile = std::getenv("HISTFILE");
+	if (historyFile)
+	{
+		HistoryManager::getInstance().writeHistoryToFile(historyFile);
 	}
 	exit(code);
 }
